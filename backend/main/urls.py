8a4611f5f8ed0ff9b4ef179f2e80a8d8e16c8466
argon-dev22 <hyouga.programing@gmail.com>
2024-12-03 07:03:46 +0900

    update urls.py

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+from .views import Tasks
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('tasks/', Tasks.as_view(),),
 ]
