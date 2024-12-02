8612ad1c16d2199036e4399add8fc686071fb7f1
argon-dev22 <hyouga.programing@gmail.com>
2024-12-03 08:05:57 +0900

    init front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,31 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Task {
+  id: number;
+  title: string;
+  memo: string;
+}
+
+const BACKEND_ORIGIN = import.meta.env.VITE_BACKEND_ORIGIN;
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_ORIGIN}/api/tasks/`)
+      .then((res) => setTasks(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
-      <div>hello world</div>
+      <h1>Tasks</h1>
+      {tasks.map((task) => (
+        <div key={task.id}>{task.title}</div>
+      ))}
     </>
   );
 }
