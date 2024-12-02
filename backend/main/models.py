1077112773708a994230a0c4349615cc2f9f5ab6
argon-dev22 <hyouga.programing@gmail.com>
2024-12-03 06:21:49 +0900

    tweak

--- backend/main/models.py
+++ backend/main/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+
+    def __str__(self):
+        return self.title
