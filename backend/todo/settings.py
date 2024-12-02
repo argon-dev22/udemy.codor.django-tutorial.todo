cbf10be17b389a70e3af3f5fe9f37c4cf8bc4d68
argon-dev22 <hyouga.programing@gmail.com>
2024-12-03 05:47:02 +0900

    tweaks

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -49,9 +49,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates',
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
