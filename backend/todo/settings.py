805546940ab2ba6f808ada3fc18823cbb12af6ef
argon-dev22 <hyouga.programing@gmail.com>
2024-12-03 05:57:38 +0900

    fix setting.py

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo.apps.TodoConfig',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
