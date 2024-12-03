d7cc83a52c0178a01418e060427a506c38659b2a
argon-dev22 <hyouga.programing@gmail.com>
2024-12-03 09:38:01 +0900

    add TaskDetail

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Tasks
+from .views import TaskList
+from .views import TaskDetail
 
 urlpatterns = [
-    path('tasks/', Tasks.as_view()),
+    path('tasks/', TaskList.as_view()),
+    path('tasks/<int:pk>/', TaskDetail.as_view()),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,11 +1,37 @@
 from django.views.generic.list import ListView
-from .models import Task
+from django.views.generic.detail import DetailView
 from django.http import JsonResponse
 
-class Tasks(ListView):
+from .models import Task
+
+class TaskList(ListView):
     model = Task
 
     def get(self, request, *args, **kwargs):
-        tasks = Task.objects.all().values()
-        return JsonResponse(list(tasks), safe=False)
+        tasks_data = Task.objects.all()
+        tasks = self.to_list(tasks_data)
+        return JsonResponse(tasks, safe=False)
+    
+    def to_list(self, tasks_data):
+        return list(map(lambda task: {
+            "id": task.id,
+            "title": task.title,
+            "memo": task.memo,
+        }, tasks_data))
 
+
+class TaskDetail(DetailView):
+    model = Task
+
+    def get(self, request, *args, **kwargs):
+        task_id = self.kwargs.get('pk')
+        task_data = Task.objects.get(id=task_id)  
+        task = self.to_dict(task_data)
+        return JsonResponse(task)
+    
+    def to_dict(self, task_data):
+        return {
+            "id": task_data.id,
+            "title": task_data.title,
+            "memo": task_data.memo,
+        }
