9dd91f42758b8e68ff1972d3a1c93a0fa99b1a71
argon-dev22 <hyouga.programing@gmail.com>
2024-12-04 09:03:31 +0900

    add router

---
+++ frontend/src/Router.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./page/Home";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/task" element={<div>Task</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
